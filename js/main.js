890a17d390a8ae6cbdc34f46bd58262d2f03c46e
Catherinecao <catherinecao25@gmail.com>
2015-09-30 12:15:08 +1300

    hover setting

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	var $light = $('.main'),
+		$lightBar = $('.straight'),
+		$screenHeight = window.innerHeight;
+
+
+	//initialize
+
+	$light.css({"bottom":$screenHeight-80});
+	$lightBar.css({"height":$screenHeight-80});
+
+
+});
