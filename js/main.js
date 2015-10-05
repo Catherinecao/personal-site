1c5f8b8903a91c56d81954dc605624a6d89e2f35
Catherinecao <catherinecao25@gmail.com>
2015-10-05 17:33:25 +1300

    more setting

--- js/main.js
+++ js/main.js
@@ -6,15 +6,24 @@ $(document).ready(function(){
 		$contactTitle = $('.contact-title'),
 		$aboutBtn = $('.about-btn'),
 		$aboutPage = $('.about-content'),
+		$aboutLight = $('.about'),
+		$navItems = $('.nav-item'),
+		$navItemAbout = $('.1'),
+		$navItemProject = $('.2'),
+		$navItemBack = $('.3'),
 		$screenHeight = window.innerHeight,
-		$screenWidth = window.innerWidth;
+		$screenWidth = window.innerWidth,
+		movingTime = 600;
 
 
 	//initialize
 
 	$light.css({"bottom":$screenHeight-80});
 	$lightBar.css({"height":$screenHeight-80});
-	$aboutPage.css({"left":$screenWidth});
+	$navItemBack.css({"opacity":0});
+	$navItemAbout.css({"opacity":0});
+	$navItemProject.css({"opacity":0});
+	
 	
 
 	//contact box
@@ -27,7 +36,52 @@ $(document).ready(function(){
 	//about page
 	$aboutBtn.click(function(e){
 		e.preventDefault();
-		$aboutPage.css({"left":0});
+
+		aboutShow();
+
+		aboutLightHide();
+
+		ProjectShow();		
 	});
+
+	//back
+	$navItemBack.click(function(e){
+		e.preventDefault();
+		$navItems.animate({opacity:0},200,'easeOutExpo');
+		aboutHide();
+		aboutLightShow();
+
+	});
+
+
+	function aboutShow (){
+		$aboutPage.animate({left:$screenWidth*0.7},{duration:movingTime},"easeOutBounce");
+	}
+
+	function aboutHide (){
+		$aboutPage.animate({left:0},{duration:movingTime},"easeInBounce");
+	}
+
+	function aboutLightHide (){
+		$aboutLight.animate({top:$screenHeight},{duration:300},"easeInBack");
+	}
+	function aboutLightShow (){
+		$aboutLight.animate({top:0},{duration:300},"easeInBack");
+	}
+
+	function ProjectShow (){
+		$navItemBack.delay(1000).animate(
+			{opacity:1,top:20},
+			{duration:{opacity:200,top:300},
+			specialEasing:{opacity:'easeOutExpo',top:'easeOutBounce'}
+			}
+		);
+		$navItemProject.delay(1500).animate(
+			{opacity:1,top:60},
+			{duration:{opacity:200,top:300},
+			specialEasing:{opacity:'easeOutExpo',top:'easeOutBounce'}
+			}
+		);
+	}
 
 });
