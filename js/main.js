5f8aff3a0cdd99beba5595110a9153d2448c83f0
Catherinecao <catherinecao25@gmail.com>
2015-10-08 12:17:26 +1300

    more setting

--- js/main.js
+++ js/main.js
@@ -11,8 +11,10 @@ $(document).ready(function(){
 		$navItemAbout = $('.1'),
 		$navItemProject = $('.2'),
 		$navItemBack = $('.3'),
-		$pBtn = $('.p-btn'),
-		$nBtn = $('.n-btn'),
+		$projectPage = $('.project-content'),
+		$projectContral = $('.light'),
+		$pBtn = $('#p-btn'),
+		$nBtn = $('#n-btn'),
 		$screenHeight = window.innerHeight,
 		$screenWidth = window.innerWidth,
 		movingTime = 600;
@@ -38,22 +40,31 @@ $(document).ready(function(){
 
 		aboutLightHide();
 
-		projectShow();		
+		btnShow();		
 	});
 
 	//back
 	$navItemBack.click(function(e){
 		e.preventDefault();
 		
+		btnHide();
 		projectHide();
 		aboutHide();
 		aboutLightShow();
 
 	});
 
+	//prpject
+	$navItemProject.click(function(e){
+		e.preventDefault();
+
+		aboutHide();
+		projectShow();
+		aboutBtnShow();
+	});
 
 	function aboutShow (){
-		$aboutPage.animate({left:$screenWidth*0.7},{duration:movingTime},"easeOutBounce");
+		$aboutPage.animate({left:$screenWidth*0.7+20},{duration:movingTime},"easeOutBounce");
 	}
 
 	function aboutHide (){
@@ -67,22 +78,22 @@ $(document).ready(function(){
 		$aboutLight.animate({top:0},{duration:300},"easeInBack");
 	}
 
-	function projectShow (){
+	function btnShow (){
 		$navItemBack.addClass('move-in').delay(1000).animate(
-			{opacity:1,top:20},
+			{opacity:1,top:40},
 			{duration:{opacity:200,top:300},
 			specialEasing:{opacity:'easeOutExpo',top:'easeOutBounce'}
 			}
 		);
 		$navItemProject.addClass('move-in').delay(1500).animate(
-			{opacity:1,top:60},
+			{opacity:1,top:80},
 			{duration:{opacity:200,top:300},
 			specialEasing:{opacity:'easeOutExpo',top:'easeOutBounce'}
 			}
 		);
 	}
 
-	function projectHide (){
+	function btnHide (){
 		$navItemProject.animate(
 			{opacity:0,top:0},
 			{duration:{opacity:100,top:200},
@@ -91,6 +102,28 @@ $(document).ready(function(){
 			{opacity:0,top:0},
 			{duration:{opacity:100,top:200},
 			specialEasing:{opacity:'easeOutExpo',top:'easeOutBounce'}}).removeClass('move-in');
+	}
+
+	function projectShow (){
+		$projectPage.animate({left:$screenWidth*0.7+20},{duration:movingTime},"easeOutBounce");
+		projectContralShow();
+
+	}
+	function projectHide (){
+		$projectPage.animate({left:0},{duration:movingTime},"easeInBounce");
+		$projectContral.removeClass('active');
+
+	}
+	function projectContralShow (){
+		$projectContral.addClass('active');
+		
+	}
+
+	function aboutBtnShow (){
+		$navItemProject.animate(
+			{opacity:0,top:90},
+			{duration:{opacity:200,top:400},
+			specialEasing:{opacity:'easeOutExpo',top:'easeOutExpo'}}).removeClass('move-in');
 	}
 
 });
